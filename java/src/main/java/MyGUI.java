09042293ca4e0ecc23cb4a656b06643766bdf290
Jacob Koch <jacobtkoch@gmail.com>
2016-03-30 11:17:04 -0700

    Add class stuff

--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
 import java.util.*;
 
 /**
@@ -8,22 +9,36 @@ import java.util.*;
 public class MyGUI extends JFrame {
 	
 	public MyGUI() {
-		super("The Skywalker Family");
+		super("The GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton theButton = new JButton("Click me!");
+        theButton.addActionListener(new ActionListener() {
+            //class body goes here
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You clicked me!");
+            }
+        });
 
+        JButton theButton2 = new JButton("No, click me!");
+        theButton2.addActionListener(new ActionListener() {
+            //class body goes here
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Haha, I win!");
+            }
+        });
 
+        this.setLayout(new FlowLayout());
 
-
-		
 		this.add(theButton);
+        this.add(theButton2);
 		
 		this.pack();
 		this.setVisible(true);		
 	}
+    
 	
 	public static void main(String[] args) {
-    new MyGUI(); //create the frame
-  }
+        new MyGUI(); //create the frame
+    }
 }
